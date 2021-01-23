b0889d075bd4259c33f989e1c28031ccbd3eed27
seiwailai <seiwailai9@live.co.uk>
2021-01-23 19:54:20 +0800

    config force ssl

--- Ecommerce/settings.py
+++ Ecommerce/settings.py
@@ -28,7 +28,7 @@ if os.path.isfile(dotenv_file):
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['fruit-store-web.herokuapp.com', '127.0.0.1']
 
